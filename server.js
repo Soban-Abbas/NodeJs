c492aa60fd7b9c38a5065a53a9a902f15b9c30b6
Soban Abbas <sobanabbass@gmail.com>
2026-03-04 19:26:10 +0500

    assign 1: server and routing with nodejs

--- server.js
+++ server.js
@@ -1,55 +1,52 @@
-const http = require('http')
+
 const fs = require('fs');
-const { buffer } = require('stream/consumers');
+const http = require('http');
+
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+
     if (req.url === '/') {
-        res.setHeader("Content-Type", "text/html");
-        res.write('<html>');
-        res.write('<head><title>Form</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message" Placeholder="Enter Username"><br> <Button type="submit">Submit</Button></form></body>');
-        res.write('</html>');
-        return res.end()
+        res.setHeader('Content-Type', 'text/html')
+        res.statusCode = 200;
+        res.write('<html>')
+        res.write('<head>');
+        res.write('<title> website</title>')
+        res.write('<body><h1>WellCome to My Website</h1>')
+        res.write('<form action="/create-user" method="POST">')
+        res.write('<input type="text" name="username" Placeholder="Enter Username"><br>');
+        res.write('<button type ="submit">Submit</button></form></body')
+        res.write('</head>')
+        res.write('</html>')
+      return  res.end();
     }
-    if (req.url === '/message' && req.method === "POST") {
-        let data = [];
-        req.on("data", (chunk) => {
-            data.push(chunk);
-
+    if (req.url === '/users') {
+        res.setHeader('Content-Type', 'text/html')
+        res.statusCode = 200;
+        res.write('<html>')
+        res.write('<head>');
+        res.write('<title> website</title>')
+        res.write('<body><h1>User of webiste</h1>')
+        res.write('<ul><li>user1</li><li>user2</li><li>user3</li><li>user4</li></ul></body>')
+        res.write('</head>')
+        res.write('</html>')
+     return   res.end();
+    }
+    if (req.url === '/create-user' && req.method === 'POST') {
+        let dataArray = [];
+        req.on('data', (chunk) => {
+            dataArray.push(chunk);
         })
-
-        req.on("end", () => {
-            let parsedata = Buffer.concat(data)
-            let dataString = parsedata.toString();
-
+        req.on('end', () => {
+            let parseData = Buffer.concat(dataArray);
+            let dataString = parseData.toString();
             let value = dataString.split('=')[1];
-            console.log(value);
-            // fs.writeFileSync('database.txt', value);//instead of writing this we should use writefile becasue it not block code execution
-            fs.writeFile('database', value, (err) => {
-                if (err) {
-                    res.write(err);
-                    res.end();
-                } else {
-                    res.statusCode = 302;//(redirect status code )
-                    res.setHeader("Location", "/");/*//Ye browser ko instruction deta hai:
+            fs.writeFile('database.txt', value, (error) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+              return  res.end();
+            });
 
-                     “Next request / (home route) par bhejo.”
-
-                     Lekin sirf ye line alone kuch nahi karti.
-                     Ye tab kaam karti hai jab redirect status code bhi ho jo oper dya huia ha res.statusCode=302.*/
-                    res.end();
-                }
-            })
         })
-
     }
-    res.setHeader("Content-type", "text/html");
-    res.write('<html>')
-    res.write('<head><title>Response</title></head>')
-    res.write('<body>Respose from server</body>')
-    res.write('</html>')
-    res.end();
 
 })
-
 server.listen(3000);
