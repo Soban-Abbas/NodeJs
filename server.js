ebe06ffe00fba9d86ccfe705af5345db57b11f1a
Soban Abbas <sobanabbass@gmail.com>
2026-03-06 11:41:10 +0500

    Assign 2 working with middlewere

--- server.js
+++ server.js
@@ -1,52 +1,32 @@
+const express=require('express');
+const app=express();
 
-const fs = require('fs');
-const http = require('http');
 
-const server = http.createServer((req, res) => {
+//part2 koie b req aye 2 middlewere sy guzray or console and res send kry
 
-    if (req.url === '/') {
-        res.setHeader('Content-Type', 'text/html')
-        res.statusCode = 200;
-        res.write('<html>')
-        res.write('<head>');
-        res.write('<title> website</title>')
-        res.write('<body><h1>WellCome to My Website</h1>')
-        res.write('<form action="/create-user" method="POST">')
-        res.write('<input type="text" name="username" Placeholder="Enter Username"><br>');
-        res.write('<button type ="submit">Submit</button></form></body')
-        res.write('</head>')
-        res.write('</html>')
-      return  res.end();
-    }
-    if (req.url === '/users') {
-        res.setHeader('Content-Type', 'text/html')
-        res.statusCode = 200;
-        res.write('<html>')
-        res.write('<head>');
-        res.write('<title> website</title>')
-        res.write('<body><h1>User of webiste</h1>')
-        res.write('<ul><li>user1</li><li>user2</li><li>user3</li><li>user4</li></ul></body>')
-        res.write('</head>')
-        res.write('</html>')
-     return   res.end();
-    }
-    if (req.url === '/create-user' && req.method === 'POST') {
-        let dataArray = [];
-        req.on('data', (chunk) => {
-            dataArray.push(chunk);
-        })
-        req.on('end', () => {
-            let parseData = Buffer.concat(dataArray);
-            let dataString = parseData.toString();
-            let value = dataString.split('=')[1];
-            fs.writeFile('database.txt', value, (error) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-              return  res.end();
-            });
+//path in middlewere is optional
 
-        })
-    }
-
+app.use((req,res,next)=>{
+    console.log("In a 1st middlewere");
+    next();
 })
-server.listen(3000);
+app.use((req,res,next)=>{
+    console.log("In a 2nd middlewere");
+
+    res.send(`Wellcome to ${req.url}`);
+})
+
+
+//part-3
+
+app.use('/users',(req,res,next)=>{
+console.log("In a users middlewere");
+res.send("res from user midlewere")
+})
+app.use('/',(req,res,next)=>{
+    console.log("in a / middlewere");
+
+    res.send("res from / middlewere");
+})
+
+app.listen(3000);
