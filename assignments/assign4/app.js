c42285e7ee212e0ac1fbe65e3f752f8140482501
Soban Abbas <sobanabbass@gmail.com>
2026-03-09 20:26:27 +0500

    assign-4 routing dynamic content load throw ejs

--- assignments/assign4/app.js
+++ assignments/assign4/app.js
@@ -1,12 +1,17 @@
 const express=require("express");
 const app=express();
 const userRouter=require("./routes/users");
+const bodyParser=require("body-parser")
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine" ,"ejs");
 app.set('views',"templete")
 app.use(express.static("./public"))
 app.use(userRouter);
 app.use("/",(req,res)=>{
-    res.render("404");
+    res.render("404",{
+        pageTitle:"404",
+        url:req.url
+    });
 })
 
 app.listen(3000);
--- assignments/assign4/routes/users.js
+++ assignments/assign4/routes/users.js
@@ -1,18 +1,28 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
 
-const users=[];
 
-router.get("/",(req,res)=>{
-    res.render("all-users")
+const users = [];
+
+router.get("/", (req, res) => {
+    res.render("all-users", {
+        pageTitle: "Home",
+        url: req.url,
+        registerUser: users
+    })
 })
 
-router.get("/users",(req,res)=>{
-    res.render("add-users")
+router.get("/users", (req, res) => {
+    res.render("add-users", {
+        pageTitle: "Add-User",
+        url: req.url
+    })
 })
-router.post("/add-users",(req,res)=>{
-    
+router.post("/add-users", (req, res) => {
+    users.push({
+        name: req.body.username
+    })
     res.redirect('/');
 })
 
-module.exports=router;
+module.exports = router;
