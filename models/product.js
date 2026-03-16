469cfbd8bbda8e0bad48a38cb1470c9e0677b404
Soban Abbas <sobanabbass@gmail.com>
2026-03-16 12:13:48 +0500

    delete cart product if admin delete products from product-list

--- models/product.js
+++ models/product.js
@@ -2,8 +2,21 @@ const fs = require("fs");
 const path = require("path")
 const rootPath = require("../util/rootPath");
 const filePath = path.join(rootPath, "data", "Dbfile.json")
+//const cartModel=require("../models/cart");
+const cartfile=path.join(rootPath,'data','cart.json')
 
 
+const readCartFile=(cb)=>{
+      fs.readFile(cartfile,(err,cartData)=>{
+                if(err || !cartData||cartData.length===0){
+                    cb({products:[],Total:0})
+                }else{
+                    cb(JSON.parse(cartData.toString()))
+                }
+             })
+
+}
+   
 const readFileContent = (cb) => {
     fs.readFile(filePath, (err, filedata) => {
         if (err || !filedata.length) {
@@ -79,8 +92,22 @@ module.exports = class product {
     static deleteProduct(id){
         readFileContent((product)=>{
              const deleteProductIndex=product.findIndex(p=>p.id===id);
-             product.splice(deleteProductIndex,1);
+              readCartFile((cart)=>{
+                
+                const deleteFromCartIndex=cart.products.findIndex(p=>p.id===id);
+                if(deleteFromCartIndex>=0){
+                    cart.products.splice(deleteFromCartIndex,1);
+                    cart.Total=cart.products.reduce((acc,curr)=>{
+                        return acc+curr.price;
+                    },0)
+                    fs.writeFile(cartfile,JSON.stringify(cart),(err)=>{
+                        console.log(err);
+                    })
+                }
 
+              })
+             product.splice(deleteProductIndex,1);
+    
              fs.writeFile(filePath,JSON.stringify(product),(err)=>{
                 console.log(err);
              })
