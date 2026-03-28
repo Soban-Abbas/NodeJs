b27c51aa1f13b5d109816250adb18f367340ea27
Soban Abbas <sobanabbass@gmail.com>
2026-03-28 21:43:45 +0500

    just setup and use mysql databases

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const app=express();
+require('dotenv').config()
 const shop=require("./routes/shop.js")
 const adminRoute=require("./routes/admin.js");
-const cartRoutes=require("./routes/cartRoutes.js")
+const cartRoutes=require("./routes/cartRoutes.js");
+const db=require("./util/dbConfig.js");
 const bodyParser=require("body-parser")
 const path=require("path");
 const error404=require("./controllers/c404.js")
@@ -20,5 +22,7 @@ app.use(shop.shopi);
 app.use('/admin',adminRoute.route);
 app.use(cartRoutes.cart)
 app.use('/',error404);
+
+db.pool.execute('select * from products').then((result)=>console.log(result[0])).catch((err)=>console.log(err));
 
 app.listen(3000);
